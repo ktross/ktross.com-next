e6e098f55647595fac025e159c1d730f1ebc3bec
Kyle Ross <kyle@ktross.com>
2021-10-03 17:37:45 -0400

    Add basic layout (#2)
    * wip
    
    * wip
    
    * add eslint
    
    * wip
    
    * wip

---
+++ components/layout.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Header from './header'
+import Footer from './footer'
+
+export default function Layout({ children }) {
+    return (
+      <>
+        <Head>
+            <title>Create Next App</title>
+            {/* <link rel="icon" href="/favicon.ico" /> */}
+        </Head>
+        <div className="font-body bg-gray-100 min-h-screen flex flex-col">
+            <Header/>
+            <mail className="flex flex-1 justify-center w-full px-6">
+                <div className="w-full max-w-screen-xl py-6">
+                    {children}
+                </div>
+            </mail>
+            <Footer/>
+        </div>
+        {/* <Footer /> */}
+      </>
+    )
+  }
