b72e0b80f7ea8e78392cad7709b9acf536b5ddd5
Kyle Ross <kyle@ktross.com>
2021-10-03 16:18:03 -0400

    wip

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGitHub, faGitlab, faStackOverflow, faStackExchange, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+// import { faGitHub, faGitlab, faStackOverflow, faStackExchange, faFacebook, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 export default function Layout({ children }) {
     return (
       <>
         {/* <Navbar /> */}
         <div class="bg-gray-100">
-            <FontAwesomeIcon icon={ faFacebook } className="h-8" />
+            {/* <FontAwesomeIcon icon={ faFacebook } className="h-8" /> */}
             <main>{children}</main>
         </div>
         {/* <Footer /> */}
