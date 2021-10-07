cac7c772c0fe79dd1c95397464b859cf9fb16a18
Kyle Ross <kyle@ktross.com>
2021-10-07 00:25:06 -0400

    Update nav-social.js

--- components/nav-social.js
+++ components/nav-social.js
@@ -5,33 +5,33 @@ import { faGitHub, faGithubAlt, faGitlab, faStackOverflow, faStackExchange, faFa
 export default function NavSocial(props) {
     return (
         <div className={['text-3xl', props.className].join(' ')}>
-            <Link href="https://github.com/ktross">
-                <a target="_blank" className={['ml-2', props.linkClass].join(' ')}>
+            <Link href="https://github.com/ktross" passHref>
+                <a target="_blank" className={['ml-2', props.linkClass].join(' ')} rel="noopener noreferrer">
                     <FontAwesomeIcon icon={ faGithubAlt } className="h-8 inline-block" />
                 </a>
             </Link>
-            <Link href="https://gitlab.com/ktross">
-                <a target="_blank" className={['ml-2', props.linkClass].join(' ')}>
+            <Link href="https://gitlab.com/ktross" passHref>
+                <a target="_blank" className={['ml-2', props.linkClass].join(' ')} rel="noopener noreferrer">
                     <FontAwesomeIcon icon={ faGitlab } className="h-8 inline-block" />
                 </a>
             </Link>
-            <Link href="https://stackoverflow.com/users/158816/ktross">
-                <a target="_blank" className={['ml-2', props.linkClass].join(' ')}>
+            <Link href="https://stackoverflow.com/users/158816/ktross" passHref>
+                <a target="_blank" className={['ml-2', props.linkClass].join(' ')} rel="noopener noreferrer">
                     <FontAwesomeIcon icon={ faStackOverflow } className="h-8 inline-block" />
                 </a>
             </Link>
-            <Link href="https://stackexchange.com/users/53133/ktross">
-                <a target="_blank" className={['ml-2', props.linkClass].join(' ')}>
+            <Link href="https://stackexchange.com/users/53133/ktross" passHref>
+                <a target="_blank" className={['ml-2', props.linkClass].join(' ')} rel="noopener noreferrer">
                     <FontAwesomeIcon icon={ faStackExchange } className="h-8 inline-block" />
                 </a>
             </Link>
-            <Link href="https://www.facebook.com/kyle.t.ross/">
-                <a target="_blank" className={['ml-2', props.linkClass].join(' ')}>
+            <Link href="https://www.facebook.com/kyle.t.ross/" passHref>
+                <a target="_blank" className={['ml-2', props.linkClass].join(' ')} rel="noopener noreferrer">
                     <FontAwesomeIcon icon={ faFacebookSquare } className="h-8 inline-block" />
                 </a>
             </Link>
-            <Link href="https://www.linkedin.com/in/kyleross/">
-                <a target="_blank" className={['ml-2', props.linkClass].join(' ')}>
+            <Link href="https://www.linkedin.com/in/kyleross/" passHref>
+                <a target="_blank" className={['ml-2', props.linkClass].join(' ')} rel="noopener noreferrer">
                     <FontAwesomeIcon icon={ faLinkedin } className="h-8 inline-block" />
                 </a>
             </Link>
