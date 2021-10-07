c3cfbab94579814146ca3482b661035d17828624
Kyle Ross <kyle@ktross.com>
2021-10-07 00:19:22 -0400

    Update bio.js

--- components/bio.js
+++ components/bio.js
@@ -4,7 +4,7 @@ export default function Bio() {
     return (
         <div className="flex space-x-14 my-14">
             <div>
-                <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Hi there! I'm Kyle Ross, a <span className="text-cinnabar-500">Full Stack Engineer</span> with over <span title={experienceStart + ' to present'}>{experience} years</span> of experience</h3>
+                <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Hi there! I&apos;m Kyle Ross, a <span className="text-cinnabar-500">Full Stack Engineer</span> with over <span title={experienceStart + ' to present'}>{experience} years</span> of experience</h3>
                 <div className="mt-5 prose-lg prose-indigo text-gray-600 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
                     <p>Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget pretium arcu vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna. Bibendum eu nulla feugiat justo, elit adipiscing. Ut tristique sit nisi lorem pulvinar. Urna, laoreet fusce nibh leo. Dictum et et et sit. Faucibus sed non gravida lectus dignissim imperdiet a.</p>
                     <p>Dictum magnis risus phasellus vitae quam morbi. Quis lorem lorem arcu, metus, egestas netus cursus. In.</p>
