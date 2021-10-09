868801ad53059598e9968d3bd4ae0b8d55d2cfdd
Kyle Ross <kyle@ktross.com>
2021-10-09 08:21:03 -0400

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -9,7 +9,7 @@ export default function Layout({ children }) {
             <title key="title">Layout</title>
             {/* <link rel="icon" href="/favicon.ico" /> */}
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
             <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap" rel="stylesheet"/>
         </Head>
         <div className="font-body bg-gray-100 min-h-screen md:flex md:flex-col w-full">
