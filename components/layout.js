601e708c30a54d878871bc6d0b6abce0e5025aa0
Kyle Ross <kyle@ktross.com>
2021-10-09 08:20:14 -0400

    change font

--- components/layout.js
+++ components/layout.js
@@ -8,6 +8,9 @@ export default function Layout({ children }) {
         <Head>
             <title key="title">Layout</title>
             {/* <link rel="icon" href="/favicon.ico" /> */}
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap" rel="stylesheet"/>
         </Head>
         <div className="font-body bg-gray-100 min-h-screen md:flex md:flex-col w-full">
             <Header/>
