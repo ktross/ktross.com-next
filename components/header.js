9ea81d281ffffaec5b968f7b85bac28c5e4fa683
Kyle Ross <kyle@ktross.com>
2021-10-10 02:39:24 -0400

    mobile menu, misc tweaks

--- components/header.js
+++ components/header.js
@@ -8,26 +8,35 @@ export default function Header() {
     const [isOpen, setisOpen] = React.useState(false);
     return (
         <>
-            <div className="w-full text-right bg-white p-4">
-                <button onClick={() => setisOpen(!isOpen)}>
-                    {!isOpen && (
-                        <FontAwesomeIcon className="fill-current text-black h-6" icon={ faBars }/>
-                    )}
-                    {isOpen && (
-                        <FontAwesomeIcon className="fill-current text-black h-6" icon={ faTimes }/>
-                    )}
-                </button>
-            </div>
-            <div className="md:flex text-center justify-center w-full px-6 bg-white shadow-sm">
+            <div className="md:flex text-center justify-center w-full md:px-6 bg-white shadow-sm">
                 <header className="md:flex items-center justify-between py-6 w-full max-w-screen-xl">
-                    <div className="inline-block text-4xl font-extrabold text-right leading-none tracking-wide">
-                        Kyle Ross
-                        <div className="text-sm font-normal -mt-1">Full Stack Engineer</div>
+                    <div className="flex items-center justify-between md:inline-block px-4 md:px-0">
+                        <div className="text-4xl font-extrabold text-right leading-none tracking-wide">
+                            Kyle Ross
+                            <div className="text-sm font-normal -mt-1">Full Stack Engineer</div>
+                        </div>
+                        <button onClick={() => setisOpen(!isOpen)} className="md:hidden rounded-md h-6 w-10 p-2 inline-flex items-center justify-center focus:outline-none">
+                            {!isOpen && (
+                                <FontAwesomeIcon className="fill-current text-black h-6" icon={ faBars }/>
+                            )}
+                            {isOpen && (
+                                <FontAwesomeIcon className="fill-current text-black h-6" icon={ faTimes }/>
+                            )}
+                        </button>
                     </div>
-                    <NavPrimary linkClass="hover:text-cinnabar-500"/>
-                    <NavSocial/>
+                    <div className="hidden md:block">
+                        <NavPrimary linkClass="hover:text-cinnabar-500"/>
+                    </div>
+                    <div className="hidden md:block">
+                        <NavSocial/>
+                    </div>
                 </header>
             </div>
+            {isOpen && (
+            <div className="md:hidden absolute top-26 left-0 w-full px-4 py-6 bg-white shadow-sm">
+                <NavPrimary linkClass="block py-2 hover:text-cinnabar-500"/>
+            </div>
+            )}
         </>
     )
 }
--- components/nav-social.js
+++ components/nav-social.js
@@ -3,23 +3,23 @@ import { faGithubAlt, faGitlab, faStackOverflow, faStackExchange, faFacebookSqua
 
 export default function NavSocial(props) {
     return (
-        <div className={['text-3xl', props.className].join(' ')}>
-            <a href="https://github.com/ktross" target="_blank" className={['ml-2', props.linkClass].join(' ')} rel="noopener noreferrer">
+        <div className={['flex items-center justify-end space-x-4', props.className].join(' ')}>
+            <a href="https://github.com/ktross" target="_blank" className={['', props.linkClass].join(' ')} rel="noopener noreferrer">
                 <FontAwesomeIcon icon={ faGithubAlt } className="h-8 inline-block" />
             </a>
-            <a href="https://gitlab.com/ktross" target="_blank" className={['ml-2', props.linkClass].join(' ')} rel="noopener noreferrer">
+            <a href="https://gitlab.com/ktross" target="_blank" className={['', props.linkClass].join(' ')} rel="noopener noreferrer">
                 <FontAwesomeIcon icon={ faGitlab } className="h-8 inline-block" />
             </a>
-            <a href="https://stackoverflow.com/users/158816/ktross" target="_blank" className={['ml-2', props.linkClass].join(' ')} rel="noopener noreferrer">
+            <a href="https://stackoverflow.com/users/158816/ktross" target="_blank" className={['', props.linkClass].join(' ')} rel="noopener noreferrer">
                 <FontAwesomeIcon icon={ faStackOverflow } className="h-8 inline-block" />
             </a>
-            <a href="https://stackexchange.com/users/53133/ktross" target="_blank" className={['ml-2', props.linkClass].join(' ')} rel="noopener noreferrer">
+            <a href="https://stackexchange.com/users/53133/ktross" target="_blank" className={['', props.linkClass].join(' ')} rel="noopener noreferrer">
                 <FontAwesomeIcon icon={ faStackExchange } className="h-8 inline-block" />
             </a>
-            <a href="https://www.facebook.com/kyle.t.ross/" target="_blank" className={['ml-2', props.linkClass].join(' ')} rel="noopener noreferrer">
+            <a href="https://www.facebook.com/kyle.t.ross/" target="_blank" className={['', props.linkClass].join(' ')} rel="noopener noreferrer">
                 <FontAwesomeIcon icon={ faFacebookSquare } className="h-8 inline-block" />
             </a>
-            <a href="https://www.linkedin.com/in/kyleross/" target="_blank" className={['ml-2', props.linkClass].join(' ')} rel="noopener noreferrer">
+            <a href="https://www.linkedin.com/in/kyleross/" target="_blank" className={['', props.linkClass].join(' ')} rel="noopener noreferrer">
                 <FontAwesomeIcon icon={ faLinkedin } className="h-8 inline-block" />
             </a>
         </div>
